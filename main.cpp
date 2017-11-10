2e5731f1a40a754045470d62b005d39121add951
Rafael Martinez <rafaelmtzjr@gmail.com>
2017-11-10 03:58:27 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
+#include <string>
+#include <tuple>
 #include <utility>
 #include <SFML/graphics.hpp>
+#include <SFML/audio.hpp>
 
 void title();
-void level_1();
-void controls();
-void jump_count(bool isJumping, bool isLanded);
-std::pair <float, float> updateMovement(float x, float y, float MasterPosObjX, float MasterPosObjY);
+void level1();
 
 float WindowX = 800, WindowY = 600;
 
 sf::RenderWindow window(sf::VideoMode(WindowX, WindowY), "The Great Game");
 sf::Event event;
-
-
 
 class detection
 {
@@ -25,7 +23,7 @@ public:
         if((ObjPosX > triggerTopX && ObjPosY > triggerTopY) && (triggerBottomX > ObjPosX && triggerBottomY > ObjPosY))
         {
             Triggered = true;
-            std::cout << Triggered << std::endl;
+            //std::cout << Triggered << std::endl;
             return Triggered;
         }
 /*---- Note ----*/ // it checks if the object or player is not within the boundary
@@ -35,19 +33,23 @@ public:
             //std::cout << Triggered << std::endl;
             return Triggered;
         }
+
     }
 };
+//-------------------OBJECT CLASS---------------------------------------------------------------------------//
+// OBJECT class: There is nothing to update in the object class
 
 class object
 {
 public:
-    std::pair <float, float > box(float SetPosX, float SetPosY , float sizeX, float sizeY, int R, int G, int B)
+//--------------------------------------------------------------------
+//draw box anywhere you want // DON'T TOUCH THIS AT ALL
+    std::pair <float, float > box(float *SetPosX, float *SetPosY , float sizeX, float sizeY, int R, int G, int B)
     {
         sf::RectangleShape rectan;
-        rectan.setPosition(SetPosX, SetPosY);
+        rectan.setPosition(*SetPosX, *SetPosY);
         rectan.setSize(sf::Vector2f(sizeX, sizeY));
         rectan.setFillColor(sf::Color(R, G, B));
-
         std::pair <float, float> Pos(rectan.getPosition().x, rectan.getPosition().y);
 
         window.draw(rectan);
@@ -55,7 +57,8 @@ public:
 
         return  Pos;
     }
-
+//---------------------------------------------------------------------
+//Can place a circle // DON'T TOUCH THIS AT ALL
     void circle(float SetPosX, float SetPosY, float Radius, int R, int G, int B)
     {
         sf::CircleShape circle;
@@ -64,7 +67,8 @@ public:
         circle.setFillColor(sf::Color(R, G, B));
         window.draw(circle);
     }
-
+//----------------------------------------------------------------------
+// This is used to set the position of the text and write text to screen // DON'T TOUCH THIS AT ALL!!!!!1
     std::pair <float, float> text(std::string TTF, unsigned int TextSize, std::string TypeText, float SetPosX, float SetPosY, int R, int G, int B)
     {
         sf::Font font;
@@ -89,7 +93,8 @@ public:
 
         return boundary;
     }
-
+//-----------------------------------------------------------------------
+// This is to set camera position //ALL OF THIS WORKS SO DON'T TOUCH!!!!!
     void  camera(float charPosX, float charPosY)
     {
         sf::View view;
@@ -103,8 +108,9 @@ public:
 
         window.setView(view);
     }
-
-    void sprite(std::string fileName, float SpritePosX, float SpritePosY)
+//-----------------------------------------------------------------------
+// This is to set sprites  // ALL OF THIS WORKS SO DON'T TOUCH!!!!!!!!
+    std::pair <float, float> sprite(std::string fileName, float SpritePosX, float SpritePosY, float updateMovementx, float updateMovementY)
     {
         sf::Texture texture;
         sf::Sprite sprite;
@@ -116,15 +122,65 @@ public:
 
         sprite.setTexture(texture);
         sprite.setPosition(SpritePosX, SpritePosY);
+        sprite.move(updateMovementx, updateMovementY);
         window.draw(sprite);
+
+        std::pair <float, float> PosOfSprite (SpritePosX, SpritePosY);
+
+        return PosOfSprite;
     }
 };
 
+//needs more testing
+// two different class needed?
 class physics
 {
+// also contains collision
 public:
+    std::pair <float, float> gravityF (float *ObjPositionX, float *ObjPositionY, float *SizeOfObjX, float *SizeOfObjY, float posPlatformX, float posPlatformY, float SizeOfPlatformX, float SizeofPlatformY)
+    {
+        float velocityX = 0, velocityY = 0;
+        float accelerationX = 0, accelerationY = 0;
+        float gravity = 1;
 
+        *ObjPositionX += accelerationX;
+        *ObjPositionY += accelerationY;
+
+        //std::cout << ObjPositionY << std::endl;
+
+        bool offsetX = false;
+        bool first = false;
+        bool second = false;
+        bool third = false;
+        //this checks if it is on the same boundary between first point of and second point of x
+        if((*ObjPositionX > posPlatformX + SizeOfPlatformX || *ObjPositionX + *SizeOfObjX < posPlatformX ) )
+        {
+            offsetX = true;
+            first = true;
+            std::cout << "first is active: " << first << std::endl;
+            *ObjPositionY += gravity;
+        }
+        // this checks if it out of bounds of the platform
+        else if(*ObjPositionY + *SizeOfObjY< posPlatformY && offsetX == false)
+        {
+            *ObjPositionY += gravity;
+            bool second = true;
+            std::cout << "second is active: " << second << std::endl;
+            //std::cout << "it works " << ObjPositionY << std::endl;
+        }
+
+        //this keeps it from sinking from the platform
+        else if (*ObjPositionY + *SizeOfObjY  > posPlatformY)
+        {
+            *ObjPositionY = posPlatformY + *SizeOfObjY;
+            std::cout << "triggered" << std::endl;
+        }
+        std::pair <float, float> PositionUpdate(*ObjPositionX, *ObjPositionY);
+        return PositionUpdate;
+    }
 };
+
+
 
 int main()
 {
@@ -137,9 +193,7 @@ void title()
 {
     while (window.isOpen())
     {
-
-        bool clicked = false;
-
+        bool clicked;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
@@ -154,17 +208,13 @@ void title()
         detection *triggerBox;
 
         window.clear();
-        //The objects that will printed to the screen
+
         win->circle(180, 120, 80.0f, 0, 0, 255);
         win->text("arial", 45, "The Great Game", 240, 180, 255, 0, 0);
         //This makes the button
         std::pair <float, float> boundaryPlay = win->text("arial", 45, "Play Game", 290, 300, 255, 0, 0);
 
-        //std::cout << boundaryPlay.first << " " << boundaryPlay.second << std::endl;
-
         sf::Vector2i mouse = sf::Mouse::getPosition(window);
-
-        //std::cout << mouse.x << " " << mouse.y << std::endl;
 
         bool button = triggerBox->triggerBox(mouse.x, mouse.y, 290, 300, (290 + boundaryPlay.first), (300 + boundaryPlay.second));
 
@@ -174,127 +224,47 @@ void title()
         }
         if(clicked == true && button == true)
         {
-            level_1();
+            std::cout << "it works" << std::endl;
+            level1();
         }
         window.display();
     }
 }
+float BoxPosX = 0;
+float BoxPosY = 100;
+float platformX = 300, platformY = 500;
+float sizeBoxX = 50;
+float sizeBoxY = 50;
+float sizeofPlatormX = 400;
+float sizeofPlatormY = 40;
 
-float velocityX = 0, velocityY = 0;
-float accelerationX = 0, accelerationY = 0;
-float gravity = 1;
 
-float CharMoveX = 0;
-float CharMoveY = 0;
+float platform1X = 100, platform1Y = 550;
+float sizeofPlatorm1X = 400, sizeofPlatorm1Y = 40;
 
-float sizeCharX = 40;
-float sizeCharY = 40;
-float CharPosX = 50;
-float CharPosY = 460;
 
-    object *object;
-    sf::RectangleShape newChar;
-
-    bool isJumping = false;
-    bool isLanded = true;
-    bool Max_jump = false;
-
-void level_1()
+void level1()
 {
-    //std::cout << "loaded level 1" << std::endl;
-
-    newChar.setPosition(CharMoveX, CharPosY);
-    newChar.setSize(sf::Vector2f(sizeCharX, sizeCharY));
-    newChar.setFillColor(sf::Color::Red);
-
-    sizeCharX = newChar.getGlobalBounds().width;
-    sizeCharY = newChar.getGlobalBounds().height;
-
-    while(window.isOpen())
+     while (window.isOpen())
     {
-        while(window.pollEvent(event))
+        while (window.pollEvent(event))
         {
-            if(event.type == sf::Event::KeyPressed)
-            {
-                if(event.key.code == sf::Keyboard::Space && Max_jump == false)
-                {
-                    velocityY = -20;
-                    isJumping = true;
-                    isLanded = false;
-                }
-            }
 
-           if(event.type == sf::Event::Closed)
-           {
-              window.close();
-           }
-        };
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        object *platform;
+        object *fallingObj;
+        physics *AddGravity;
 
         window.clear();
-        object->box(0,500, 800, 300, 0, 255, 0); // green box
 
-        controls();
-        jump_count(isJumping, isLanded);
-        std::pair <float, float> PosUpdate = updateMovement(CharPosX, CharPosY, 0, 0 );
-
-        std::cout << PosUpdate.first << " " << PosUpdate.second << std::endl;
-
-        object->camera(PosUpdate.first, PosUpdate.second);
-
-        newChar.setPosition(PosUpdate.first, PosUpdate.second);
-
-        window.draw(newChar);
+        std::pair <float, float> newPosofBox = AddGravity->gravityF(&BoxPosX, &BoxPosY, &sizeBoxX, &sizeBoxY, platformX, platformY, sizeofPlatormX, sizeofPlatormY);
+        std::cout << newPosofBox.first << " " << newPosofBox.second << std::endl;
+        fallingObj->box(&newPosofBox.first,&newPosofBox.second, sizeBoxX, sizeBoxY, 255, 0, 255);
+        //AddGravity->gravityF(&BoxPosX, &BoxPosY, &sizeBoxX, &sizeBoxY, WindowX, WindowY, platform1X, platform1Y, sizeofPlatorm1X, sizeofPlatorm1Y);
+        platform->box(&platformX, &platformY, sizeofPlatormX,sizeofPlatormY , 0, 255, 0);
 
         window.display();
-    }
-}
-
-void controls()
-{
-        if(!sf::Keyboard::isKeyPressed(sf::Keyboard::D)){velocityX = 0;}
-
-        if(!sf::Keyboard::isKeyPressed(sf::Keyboard::A)){velocityX = 0;}
-
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)){velocityX = 5;}
-
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)){velocityX = -5;}
-}
-
-std::pair <float, float> updateMovement(float x, float y, float MasterPosObjX, float MasterPosObjY)
-{
-    //std::cout << x << " " << y << std::endl;
-    //::cout << x << " " << y << std::endl;
-    velocityX += accelerationX;
-    velocityY += accelerationY;
-
-
-
-    CharPosX += velocityX;
-    CharPosY += velocityY;
-
-    if(460 > CharPosY)
-    {
-        velocityY += gravity;
-    }
-    else if (CharPosY >= 460)
-    {
-        CharPosY = 460;
-        isLanded = true;
-    }
-
-    std::pair <float, float> newPos (x, y);
-
-    return newPos;
-}
-
-void jump_count(bool isJumping, bool isLanded)
-{
-    if(isJumping == true)
-    {
-        Max_jump = true;
-        if(isLanded == true)
-        {
-            Max_jump = false;
-        }
     }
 }
